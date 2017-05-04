da0fb823d931e4aba91a0cfee31454ff0a160d13
nanhung <nhsieh@cvm.tamu.edu>
2017-05-04 12:56:02 -0500

    test converge tce.2.12

--- TCE_post.R
+++ TCE_post.R
@@ -7,5 +7,5 @@ if(!require(readr)) {
 }
 
 # 
-TCE.2.12_v1 <- as.data.frame(read_table2("TCE.2.12.pop_1.out"))
-TCE.2.12_v2 <- as.data.frame(read_table2("TCE.2.12.pop_2.out"))
+TCE.2.12_v1 <- as.data.frame(fread("TCE.2.12.pop_1.csv"))
+TCE.2.12_v2 <- as.data.frame(fread("TCE.2.12.pop_2.csv"))
