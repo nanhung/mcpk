8517359b78f9e8f3161c66df49893398ed5bc920
nanhung <nhsieh@cvm.tamu.edu>
2017-05-09 12:03:49 -0500

    update

--- TCE_post.R
+++ TCE_post.R
@@ -9,5 +9,3 @@ if(!require(readr)) {
 # 
 TCE.2.12_v1 <- as.data.frame(fread("TCE.2.12.pop_1.csv"))
 TCE.2.12_v2 <- as.data.frame(fread("TCE.2.12.pop_2.csv"))
-TCE.2.12_v3 <- as.data.frame(fread("TCE.2.12.pop_3.csv"))
-TCE.2.12_v4 <- as.data.frame(fread("TCE.2.12.pop_4.csv"))
