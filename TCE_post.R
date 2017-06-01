ed70c1e997a4da70e1b3d91da8ea1724d1ee4175
Nanhung <nan_1003@hotmail.com>
2017-06-01 09:37:55 -0500

    add previous data / group8 and 9

--- TCE_post.R
+++ TCE_post.R
@@ -10,7 +10,7 @@ if(!require(readr)) {
 #TCE.2.12_v1 <- as.data.frame(fread("TCE.2.12.pop_1.csv"))
 #TCE.2.12_v2 <- as.data.frame(fread("TCE.2.12.pop_2.csv"))
 
-TCE.2.13_v1 <- read_table2("TCE.2.13.pop_1.1.out")
-TCE.2.13_v2 <- read_table2("TCE.2.13.pop_1.2.out")
-TCE.2.13_v3 <- read_table2("TCE.2.13.pop_1.3.out")
+TCE.2.13_v1 <- read_table2("TCE.2.13.pop_1.5.out")
+TCE.2.13_v2 <- read_table2("TCE.2.13.pop_1.6.out")
+TCE.2.13_v3 <- read_table2("TCE.2.13.pop_1.7.out")
 #TCE.2.13_v4 <- read_table2("TCE.2.13.pop_2.4.out")
